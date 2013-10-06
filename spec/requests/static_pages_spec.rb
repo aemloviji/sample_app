02c14433dc47bf5f63e8556b1359b25eee29f3cb
AEMLoviji <AEMLoviji@gmail.com>
2013-10-06 23:39:00 +0500

    unknown

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,16 +4,19 @@ describe "StaticPages" do
 
  describe "Home page" do
 
+   
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
+
+   
   end
 
   describe "Help page" do
